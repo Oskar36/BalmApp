df6b19153bac16ecf0972a846bdbd7e1c72ba423
Oskar <llagunooskar@gmail.com>
2021-11-08 12:35:40 +0100

    Fragment apodo layout,activity nueva partida layout,layout activity acerca de

---
+++ app/src/main/java/com/example/balmapp/a_acercade.kt
@@ -0,0 +1,11 @@
+package com.example.balmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class a_acercade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.l_acercade)
+    }
+}
