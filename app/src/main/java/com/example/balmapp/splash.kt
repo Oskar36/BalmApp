0afa4e5f89aedde57f2e3d77e4738ed77bfaba55
ikerc <ikercaballero271@gmail.com>
2021-11-09 12:34:32 +0100

    splash menu y mas cosas

--- app/src/main/java/com/example/balmapp/splash.kt
+++ app/src/main/java/com/example/balmapp/splash.kt
@@ -1,20 +1,18 @@
-package com.example.boka2
+package com.example.balmapp
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import android.os.SystemClock
+
 
 class splash : AppCompatActivity() {
     //Este codigo sirve para que aprezca el logo al inicar la app
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        SystemClock.sleep(1000)
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, a_login::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
 
