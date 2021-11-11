09afa64f354ad4f30755ab5b72d24b1813349de6
Oskar <llagunooskar@gmail.com>
2021-11-11 12:22:08 +0100

    animacion tren,movimiento de dos ingredientes

--- app/src/main/java/com/example/balmapp/f_putxero_juego.kt
+++ app/src/main/java/com/example/balmapp/f_putxero_juego.kt
@@ -3,9 +3,12 @@ package com.example.balmapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import com.example.balmapp.databinding.LPutxeroJuegoBinding
+import android.view.animation.TranslateAnimation
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 private var _binding: LPutxeroJuegoBinding? = null
@@ -31,9 +34,40 @@ class f_putxero_juego : Fragment() {
         binding.btnPutxerojuego.setOnClickListener(){
             NavFrag.IniciarActivity(requireContext(),"a_mapa")
         }
+        val moveLefttoRight = TranslateAnimation(550F, -470F, 0F, 0F)
+        moveLefttoRight.setDuration(30000)
+        moveLefttoRight.setFillAfter(true)
+        binding.imgPutxeroTren.startAnimation(moveLefttoRight)
+        binding.imgPutxeroAlimento1.setOnTouchListener(touchListener)
+        binding.imgPutxeroAlimento5.setOnTouchListener(touchListener)
+
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    var xDelta: Int=0
+    var yDelta: Int=0
+    val touchListener = View.OnTouchListener { view, event ->
+        val x = event.rawX.toInt()
+        val y = event.rawY.toInt()
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                val lParams = view.layoutParams as ConstraintLayout.LayoutParams
+                xDelta = x - lParams.leftMargin
+                yDelta = y - lParams.topMargin
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val layoutParams = view
+                    .layoutParams as ConstraintLayout.LayoutParams
+                layoutParams.leftMargin = x - xDelta
+                layoutParams.topMargin = y - yDelta
+                layoutParams.rightMargin = 0
+                layoutParams.bottomMargin = 0
+                view.layoutParams = layoutParams
+            }
+        }
+       binding.frameLayout11.invalidate()
+        true
     }
 }
