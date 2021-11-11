f35b5e02300adc137cae19e0041832b5f6bef860
iker <ikercaballero271@gmail.com>
2021-11-11 12:50:22 +0100

    audio de santiago

--- app/src/main/java/com/example/balmapp/f_felipe_cancion.kt
+++ app/src/main/java/com/example/balmapp/f_felipe_cancion.kt
@@ -1,5 +1,6 @@
 package com.example.balmapp
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,7 +16,7 @@ private var _binding: LFelipeCancionBinding? = null
 // This property is only valid between onCreateView and
 // onDestroyView.
 private val binding get() = _binding!!
-
+var mediaPlayer: MediaPlayer?=null
 class f_sanfelipe_cancion : Fragment() {
 
 
@@ -38,8 +39,31 @@ class f_sanfelipe_cancion : Fragment() {
             NavFrag.IniciarActivity(requireContext(),"a_mapa")
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+         mediaPlayer = MediaPlayer.create(context, R.raw.felipe_cancion)
+
+       binding.play.setOnClickListener{
+           if(mediaPlayer!!.isPlaying()){
+               mediaPlayer!!.seekTo(0);
+           } else {
+               mediaPlayer!!.start();
+           }
+       }
+        binding.pause.setOnClickListener{
+            mediaPlayer!!.pause()
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer!!.stop()
     }
 }
