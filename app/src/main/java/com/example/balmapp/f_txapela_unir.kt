f2c6baade1490a5d872dfe6a895acee970203d70
ALBA <olanoalba@gmail.com>
2021-11-30 09:09:52 +0100

    audios txapela

--- app/src/main/java/com/example/balmapp/f_txapela_unir.kt
+++ app/src/main/java/com/example/balmapp/f_txapela_unir.kt
@@ -13,7 +13,11 @@ import com.example.balmapp.databinding.LTxapelaUnirBinding
 
 private var _binding: LTxapelaUnirBinding? = null
 private val binding get() = _binding!!
-private var mediaplayer: MediaPlayer? = null
+private var mediaplayeraudio1: MediaPlayer? = null
+private var mediaplayeraudio2: MediaPlayer? = null
+private var mediaplayeraudio3: MediaPlayer? = null
+private var mediaplayeraudio4: MediaPlayer? = null
+
 class f_juego_txapela_unir : Fragment() {
 
     override fun onCreateView(
@@ -29,54 +33,88 @@ class f_juego_txapela_unir : Fragment() {
 
         binding.btnfinalizarPuente.setOnClickListener{
             Sharedapp.gune.gune="5.Gunea"
-            mostrarDialogoPersonalizado()
+
 
         }
         //inicio de la animacion
         NavFrag.animacion_dantzaris(binding.imglogo)
-        //Inicializamos la clase MediaPlayer asociandole el fichero de Audio
-        mediaplayer = MediaPlayer.create(context, R.raw.azalpena_unir_txapela)
-        //Iniciamos el audio
-        mediaplayer!!.start()
+
 
         //parar animacion cuando pare el audio
-        mediaplayer!!.setOnCompletionListener {
-            NavFrag.animacion_dantzaris_parar(binding.imglogo)
-        }
-        //parar y continuar el audio
-        binding.imglogo.setOnClickListener {
-            if(mediaplayer!!.isPlaying){
-                NavFrag.animacion_dantzaris_parar(binding.imglogo)
-                mediaplayer!!.stop()
-            }else{
+        //    mediaplayer!!.setOnCompletionListener {
+         //   NavFrag.animacion_dantzaris_parar(binding.imglogo)        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaplayeraudio1 = MediaPlayer.create(context, R.raw.txapelaaudio1)
+        mediaplayeraudio2 = MediaPlayer.create(context, R.raw.txapelaaudio2)
+        mediaplayeraudio3 = MediaPlayer.create(context, R.raw.txapelaaudio3)
+        mediaplayeraudio4 = MediaPlayer.create(context, R.raw.txapelaaudio4)
 
 
-                mediaplayer!!.prepare()
-                mediaplayer!!.start()
-                NavFrag.animacion_dantzaris(binding.imglogo)
+
+        //funcion para controlar audio1
+        binding.playaudio1.setOnClickListener{
+            pararaudios()
+            if(mediaplayeraudio1!!.isPlaying){
+                mediaplayeraudio1!!.seekTo(0)
+            } else {
+                mediaplayeraudio1!!.start()
             }
         }
+        binding.pauseaudio1.setOnClickListener{
+            mediaplayeraudio1!!.pause()
+        }
+
+        //funcion para controlar audio2
+        binding.playaudio2.setOnClickListener{
+            pararaudios()
+            if(mediaplayeraudio2!!.isPlaying){
+                mediaplayeraudio2!!.seekTo(0)
+            } else {
+                mediaplayeraudio2!!.start()
+            }
+        }
+        binding.pauseaudio2.setOnClickListener{
+            mediaplayeraudio2!!.pause()
+        }
+        //funcion para controlar audio3
+        binding.playaudio3.setOnClickListener{
+           pararaudios()
+            if(mediaplayeraudio3!!.isPlaying){
+                mediaplayeraudio3!!.seekTo(0)
+            } else {
+                mediaplayeraudio3!!.start()
+            }
+        }
+        binding.pauseaudio3.setOnClickListener{
+            mediaplayeraudio3!!.pause()
+        }
+        //funcion para controlar audio4
+        binding.playaudio4.setOnClickListener{
+            pararaudios()
+            if(mediaplayeraudio4!!.isPlaying){
+                mediaplayeraudio4!!.seekTo(0)
+            } else {
+                mediaplayeraudio4!!.start()
+            }
+        }
+        binding.pauseaudio4.setOnClickListener{
+            mediaplayeraudio4!!.pause()
+        }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onStart() {
-        super.onStart()
-        //Iniciamos el audio
-        mediaplayer!!.start()
+        mediaplayeraudio1!!.pause()
+        mediaplayeraudio2!!.pause()
+        mediaplayeraudio3!!.pause()
+        mediaplayeraudio4!!.pause()
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        //liberacion del productor de medios
-        mediaplayer?.release()
-        mediaplayer = null
-
-
-    }
     private fun mostrarDialogoPersonalizado(){
 
         AlertDialog.Builder(requireContext(), R.style.DialogBasicCustomStyle)
@@ -97,7 +135,6 @@ class f_juego_txapela_unir : Fragment() {
             .show()
 
     }
-<<<<<<< HEAD
     private fun pararaudios(){
 
         if (mediaplayeraudio1!!.isPlaying){
@@ -119,6 +156,4 @@ class f_juego_txapela_unir : Fragment() {
 
 
     }
-=======
->>>>>>> b42d944986ffc3a13fd9e4ed9ec4fb6e88fe6e1d
 }
